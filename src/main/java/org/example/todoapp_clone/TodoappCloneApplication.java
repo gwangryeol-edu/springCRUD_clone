3fab13e0601d127f8a17ece457cecb816b268161
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2025-11-18 17:55:35 +0900

    의존성 주입 리팩토링, 샘플데이터 생성

--- src/main/java/org/example/todoapp_clone/TodoappCloneApplication.java
+++ src/main/java/org/example/todoapp_clone/TodoappCloneApplication.java
@@ -1,7 +1,12 @@
 package org.example.todoapp_clone;
 
+import org.example.todoapp_clone.dto.TodoDto;
+import org.example.todoapp_clone.repository.TodoRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoappCloneApplication {
@@ -9,5 +14,14 @@ public class TodoappCloneApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoappCloneApplication.class, args);
 
+    }
+
+    @Bean
+    public CommandLineRunner init(ApplicationArguments args){
+        return args1 -> {
+            TodoRepository.save(new TodoDto(null, "study", "JAVA", false));
+            TodoRepository.save(new TodoDto(null, "cook", "kimbob", false));
+            TodoRepository.save(new TodoDto(null, "workout", "run", false));
+        };
     }
 }
--- src/main/java/org/example/todoapp_clone/repository/TodoRepository.java
+++ src/main/java/org/example/todoapp_clone/repository/TodoRepository.java
@@ -1,12 +1,14 @@
 package org.example.todoapp_clone.repository;
 
 import org.example.todoapp_clone.dto.TodoDto;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class TodoRepository {
     private final Map<Long, TodoDto> storage = new ConcurrentHashMap<>();
     private Long nextId = 1L;
