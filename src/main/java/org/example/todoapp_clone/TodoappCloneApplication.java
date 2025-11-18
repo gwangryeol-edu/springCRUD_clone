660e2832631d109540d28750fc5911abae705a27
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2025-11-18 14:57:10 +0900

    git remote

---
+++ src/main/java/org/example/todoapp_clone/TodoappCloneApplication.java
@@ -0,0 +1,13 @@
+package org.example.todoapp_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/todoapp_clone/TodoappCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todoapp_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
