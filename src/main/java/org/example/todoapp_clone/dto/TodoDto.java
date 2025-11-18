b6db7941fc4cff8af7fdf8d334811ddae0c3bfe5
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2025-11-18 15:23:39 +0900

    todoDto 세팅

---
+++ src/main/java/org/example/todoapp_clone/dto/TodoDto.java
@@ -0,0 +1,48 @@
+package org.example.todoapp_clone.dto;
+
+// Dto(Data Transfer Object): 계층 간 데이터 전달을 위해서 사용하는 데이터 묶음
+public class TodoDto {
+    private Long id;
+    private String title;
+    private String content;
+    private boolean completed;
+
+    public TodoDto(Long id, String title, String content, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
